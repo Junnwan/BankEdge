fbfbc903a6b5b53890d5edab6a04ebb5a80fe8d0
Junnwan <danielwan189@gmail.com>
2025-11-11 21:47:43 +0800

    blank

--- controllers/sync_controller.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template
-
-sync_bp = Blueprint('sync', __name__)
-
-@sync_bp.route('/data-sync')
-def data_sync():
-    # You can later handle sync logic here
-    return render_template('data_sync.html')
